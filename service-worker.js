2592ade87e17bdd784bed2e9a6f9b49bc766ab3c
shinobunakamura1110-netizen <shinobu.nakamura.11.10@gmail.com>
2026-08-16 00:42:44 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='ribo-price-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
